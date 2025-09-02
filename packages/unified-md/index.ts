b88dc1571a495b81c320a36f4911db8a9d2cb40c
ginlon <ginlon5241@gmail.com>
2025-09-03 00:11:27 +0800

    Feature: - 在 unified-md 中添加多个依赖以增强 Markdown 转换功能 - 更新 index.ts 以支持新的功能并处理文件输出。

--- packages/unified-md/index.ts
+++ packages/unified-md/index.ts
@@ -3,12 +3,30 @@ import rehypeStringify from "rehype-stringify";
 import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
+import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug";
+import rehypeDocument from "rehype-document";
+import rehypeFormat from "rehype-format";
+import { read, write } from "to-vfile";
+import { reporter } from "vfile-reporter";
+import remarkRetext from "remark-retext";
+import retextEnglish from "retext-english";
+import retextIndefiniteArticle from "retext-indefinite-article";
 
-const document = await fs.readFile("example.md", "utf8");
-const file = await unified()
+const file = await read("example.md");
+
+await unified()
   .use(remarkParse)
+  // @ts-expect-error: fine
+  .use(remarkRetext, unified().use(retextEnglish).use(retextIndefiniteArticle))
+  .use(remarkToc)
   .use(remarkRehype)
+  .use(rehypeSlug)
+  .use(rehypeDocument, { title: "Pluto" })
+  .use(rehypeFormat)
   .use(rehypeStringify)
-  .process(document);
+  .process(file);
 
-console.log(String(file));
+console.error(reporter(file));
+file.extname = ".html";
+await write(file);
